7ff1f1f7b7d1b2e2a5d7dc430f76022a75b940ed
EC2 Default User <ec2-user@ip-172-31-31-244.ec2.internal>
2021-04-21 13:07:45 +0000

    [Add] index controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
 class HomeController < ApplicationController
-  def books
+  def top
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/books'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top' => 'home#top'
+  resources :books
+
 end
