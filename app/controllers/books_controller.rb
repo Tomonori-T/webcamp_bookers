f5d19925c04b34892319afc3e0d09a6b35d1d4a6
EC2 Default User <ec2-user@ip-172-31-31-244.ec2.internal>
2021-04-22 08:56:42 +0000

    [Add] destroy etc.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,38 @@
 class BooksController < ApplicationController
-  
+
     def show
+      @book = Book.find(params[:id])
     end
-    
+
     def index
-      @blogs = Blog.all
+      @books = Book.all
+      @book = Book.new
     end
-    
-    def new
-      @blog = Blog.new
-    end
-  
+
     def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blogs_path  
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
     end
-    
+
     def edit
+      @book = Book.find(params[:id])
     end
-    
+
+    def update
+      book = Book.find(params[:id])
+      book.update(book_params)
+      redirect_to book_path(book.id)
+    end
+
+    def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
+    end
+
     private
-    def blog_params
-    params.require(:blog).permit(:title, :body)
+    def book_params
+    params.require(:book).permit(:title, :body)
     end
 end
